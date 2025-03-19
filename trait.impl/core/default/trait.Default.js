7f7ff9787e1e3407233855e031ccadf120d23018
Muxutruk2 <Muxutruk2@users.noreply.github.com>
2025-03-19 16:42:00 +0000

    deploy: 4c72f0841fa0b3003803a019207baed656ba9ee0

--- trait.impl/core/default/trait.Default.js
+++ trait.impl/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["braincrap_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"braincrap_rs/transpiler/struct.Transpiler.html\" title=\"struct braincrap_rs::transpiler::Transpiler\">Transpiler</a>"]]]]);
+    var implementors = Object.fromEntries([["braincrap_rs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.85.1/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"braincrap_rs/transpiler/struct.Transpiler.html\" title=\"struct braincrap_rs::transpiler::Transpiler\">Transpiler</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
